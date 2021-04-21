230711f2a20929cbd2a75bac4e3c1d93889dac49
java73core <mrvandrey@yandex.ru>
2021-04-21 20:29:11 +0400

    5. Tracker - хранилище [#475116]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -8,4 +8,15 @@ public class StartUI {
         Item currentDateTimeFormat = new Item();
         System.out.println("Текущие дата и время после форматирования: " +  currentDateTimeFormat.getCreated().format(formatter));
     }
+
+    public static void main(String[] args) {
+        Tracker tracker = new Tracker();
+        tracker.add(new Item("Тест1"));
+        tracker.add(new Item("Тест1"));
+        Item[] mas = new Item[100];
+        mas = tracker.findByName("Тест1");
+        for (int i = 0; i < mas.length; i++) {
+            System.out.println(mas[i]);
+        }
+    }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -21,5 +23,28 @@ public class Tracker {
             }
         }
         return rsl;
+    }
+
+    public Item[] findByName(String key) {
+        Item[] result = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            Item item = this.items[i];
+            if (item.getName().equals(key)) {
+                result[count++] = item;
+             }
+        }
+        return Arrays.copyOf(result, count);
+    }
+
+    public Item[] findAll() {
+        Item[] first = new Item[size];
+        int count = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i] != null) {
+                first[count++] = items[i];
+            }
+        }
+        return Arrays.copyOf(first, count);
     }
 }
