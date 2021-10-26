282f3ca43d7df4720dc73dc4236c24c7eaeac919
Andrew Mironov <mrvandrey@gmail.com>
2021-10-26 14:02:28 +0400

    1. Паспорт и жители [#212656]

--- src/main/java/ru/job4j/collection/Citizen.java
+++ src/main/java/ru/job4j/collection/Citizen.java
@@ -21,7 +21,9 @@ public class Citizen {
 
     @Override
     public boolean equals(Object o) {
-        if (this == o) return true;
+        if (this == o) {
+            return true;
+        }
         if (o == null || getClass() != o.getClass()) {
             return false;
         }
