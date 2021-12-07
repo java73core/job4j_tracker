7223cceba54c2546b24c8e6ea29ab8e102af0117
Andrew Mironov <mrvandrey@gmail.com>
2021-12-07 14:56:59 +0400

    1. Телефонный справочник на базе ArrayList [#41598]

--- src/main/java/ru/job4j/search/PhoneDictionary.java
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -11,9 +11,9 @@ public class PhoneDictionary {
 
     public ArrayList<Person> find(String key) {
         ArrayList<Person> result = new ArrayList<>();
-
         for (Person person: persons) {
-            if(person.getName().equals(key) || person.getSurname().equals(key) || person.getPhone().equals(key) || person.getAddress().equals(key)) {
+            if (person.getName().equals(key) || person.getSurname().equals(key) ||
+                    person.getPhone().equals(key) || person.getAddress().equals(key)) {
                 result.add(person);
             }
         }
