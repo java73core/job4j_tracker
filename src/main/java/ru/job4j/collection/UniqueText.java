76c92d87a33d2e874f8c94d4cb0d1ffc3fcbf969
java73core <mrvandrey@yandex.ru>
2021-12-09 22:32:02 +0400

    Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/collection/UniqueText.java
+++ src/main/java/ru/job4j/collection/UniqueText.java
@@ -10,7 +10,9 @@ public class UniqueText {
         String[] text = duplicateText.split(" ");
         HashSet<String> check = new HashSet<>(Arrays.asList(origin));
         for (String str : text) {
-             if (!check.contains(str)) { rsl = false; };
+             if (!check.contains(str)) {
+                 rsl = false;
+             };
         }
         return rsl;
     }
