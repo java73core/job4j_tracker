7bbc170480a0155c1e3e98f2c6e79cbc2fe6a60c
Andrew Mironov <mrvandrey@gmail.com>
2022-01-13 18:31:43 +0400

    4. Сортировка номера [#221284]

---
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -0,0 +1,13 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class LexSort implements Comparator<String> {
+
+    @Override
+    public int compare(String left, String right) {
+        String[] strLeft = left.split("\\.", 2);
+        String[] strRight = right.split("\\.",2);
+        return Integer.compare(Integer.parseInt(strLeft[0]), Integer.parseInt(strRight[0]));
+    }
+}
