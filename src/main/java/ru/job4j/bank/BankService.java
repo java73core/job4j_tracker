e3bc7d3a7d38471170c24545c0a3a3e58120b6ce
Andrew Mironov <mrvandrey@gmail.com>
2022-01-17 14:42:26 +0400

    3. Банковские переводы [#10038 #250807]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -4,9 +4,6 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
-
-import static javax.swing.UIManager.get;
 
 /**
  * Класс описывает функционал банковских операций
