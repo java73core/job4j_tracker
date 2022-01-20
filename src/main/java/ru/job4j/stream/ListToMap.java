fd8eb3a322482828efcc6bc611097f044b4bd3cf
Andrew Mironov <mrvandrey@gmail.com>
2022-01-20 10:50:47 +0400

    2. Список адресов [#110062 #253035]

--- src/main/java/ru/job4j/stream/ListToMap.java
+++ src/main/java/ru/job4j/stream/ListToMap.java
@@ -7,12 +7,10 @@ import java.util.stream.Collectors;
 public class ListToMap {
 
     public static Map<String, Student> convert(List<Student> list) {
-        Map<String, Student> collect = list.stream()
-                .collect(Collectors
+        return list.stream().collect(Collectors
                         .toMap(Student::getSurname, student ->
                                 student, (o1, o2) -> {
                             return o1;
                         }));
-        return collect;
     }
 }
--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -6,11 +6,11 @@ import java.util.stream.Collectors;
 
 public class Profiles {
     public static List<Address> collect(List<Profile> profiles) {
-        return profiles.stream().map(x -> x.getAddress()).collect(Collectors.toList());
+        return profiles.stream().map(Profile::getAddress).collect(Collectors.toList());
     }
 
     public static List<Address> collectSortWithoutDuplicate(List<Profile> profiles) {
-        return profiles.stream().map(profile -> profile.getAddress())
+        return profiles.stream().map(Profile::getAddress)
                 .sorted(Comparator.comparing(Address::getCity))
                 .distinct().collect(Collectors.toList());
     }
